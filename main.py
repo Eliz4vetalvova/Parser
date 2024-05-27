28b045d53b50701e00bbca9d4278527f070e3d43
Eliz4vetalvova <161493722+Eliz4vetalvova@users.noreply.github.com>
2024-05-28 03:01:27 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+
+def parsing():
+    url = "https://www.pepper.ru"
+
+    try:
+
+        driver = webdriver.Edge()
+        driver.get(url)
+        page = driver.page_source
+        soup = BeautifulSoup(page, "html.parser")
+        name = soup.findAll('strong' ,class_='thread-title') #сюда элементы, которые найти надо
+        temp = soup.findAll('span', class_='vote-temp')
+        link = soup.findAll('a', class_='thread-link')
+        for i in range(len(name)):
+            print(name[i].text)
+            print(temp[i].text.strip())
+            print(link[i].get('href') + "\n")
+
+    finally:
+        driver.quit()
+
+if name == "main":
+    parsing()
+parse()
